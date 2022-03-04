eeb244c33b4962ffa02c759b284fd54e5927af69
Максим Петербурцев <mpeterburtsev@live.ru>
2022-03-04 23:29:13 +0300

    Добавьте файлы проекта.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Давай познакомимся! Как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, " + name + ". Сегодня: " + DateTime.Today.ToShortDateString());
+            Console.ReadKey();
+        }
+    }
+}
