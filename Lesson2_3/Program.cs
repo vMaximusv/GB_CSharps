5490e0f093405e35f217ac215f79dc95710a3dcf
Максим Петербурцев <mpeterburtsev@live.ru>
2022-03-05 12:53:05 +0300

    Выполнил 2 домашнее задание

---
+++ Lesson2_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3. Определить, является ли введённое пользователем число чётным
+
+            Console.WriteLine("Введите число для проверки четности");
+            int k = Convert.ToInt32(Console.ReadLine());
+            if (k % 2 == 0)
+            {
+                Console.WriteLine($"Введенное число {k}, оно является ЧЕТНЫМ.");
+            }
+            else
+            {
+                Console.WriteLine($"Введенное число {k}, оно является НЕЧЕТНЫМ.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
